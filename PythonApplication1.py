103fb176143dd754747d45b2a785b79624a17abc
Yegortyan <yegortesla@gmail.com>
2022-02-02 20:37:41 +0300

    First commit

---
+++ PythonApplication1.py
@@ -0,0 +1,9 @@
+#Создайте функцию, которая будет считать сумму чисел в массиве.
+def summa():
+    sum = 0
+    array = [1, 2, 3, 4, 5]
+    for i in array:
+        sum += i
+    print(sum)
+summa()
+
